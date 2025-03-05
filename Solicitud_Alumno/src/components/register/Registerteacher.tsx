996176e50f17e5dbc73ee71ca23eaa5ccb77bd5c
rafaTemp <rafaluva03@gmail.com>
2025-03-05 13:53:51 +0100

    quito warning registerteacher

--- Solicitud_Alumno/src/components/register/Registerteacher.tsx
+++ Solicitud_Alumno/src/components/register/Registerteacher.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { registerTeacher, registerUser } from '../../service/authServide';
+import { registerTeacher } from '../../service/authServide';
 import toast from 'react-hot-toast';
 
 export default function RegisterTeacher() {
